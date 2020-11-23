8a9e266582692d51bf2b2667ddf2e54cd9c3e1a6
Steve Carmel <steve2christ@users.noreply.github.com>
2020-11-23 02:53:14 +0100

    #1 anamnese page added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core'
+import { RouterModule, Routes } from '@angular/router'
+import { HomeComponent } from './home/home.component'
+import { AdmissionComponent } from './admission/admission.component'
+import { AdministrationComponent } from './administration/administration.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', pathMatch: 'full', redirectTo: '/home'},
+  {path: 'home', component: HomeComponent},
+  {path: 'aufnahme', component: AdmissionComponent},
+  {path: 'administration', component: AdministrationComponent}
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
