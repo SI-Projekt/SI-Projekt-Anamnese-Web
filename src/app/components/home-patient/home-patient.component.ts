0a4436122122f4511664335a882460f029a8dde2
Juvniele <fokamnji@th-brandenburg.de>
2021-01-03 22:32:30 +0100

    #4 implement_home_page_for_Patient is done

--- src/app/components/home-patient/home-patient.component.ts
+++ src/app/components/home-patient/home-patient.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit, Output } from '@angular/core'
 import { rootingPath } from '../../shared/rooting-path'
 
+// @ts-ignore
+import personsJson from '../../shared/data/person-list.json'
+import { SessionService } from '../../core/authentification-and-authority/session.service'
+import { IPerson } from '../../model/person.interface'
+
 @Component({
   selector: 'app-home-patient',
   templateUrl: './home-patient.component.html',
@@ -8,16 +13,30 @@ import { rootingPath } from '../../shared/rooting-path'
 })
 export class HomePatientComponent implements OnInit {
   headerTitle: string = 'Home - Willkomen!'
+  currentUser: IPerson = <IPerson>{}
 
   readonly aufnahme_path: string
   readonly patient_info_view_path: string
 
-  constructor() {
+  constructor(
+    private sessionService: SessionService
+  ) {
     this.aufnahme_path = '/' + rootingPath.aufnahme
     this.patient_info_view_path = '/' + rootingPath.patient_info_view
   }
 
   ngOnInit(): void {
+    this.getCurrentUser()
   }
 
+  openMyProfilModal(): void {
+  }
+
+  private getCurrentUser(): void {
+    const username = this.sessionService.getUsername()
+    this.currentUser = personsJson.filter(
+      (patient: IPerson) => patient.userName === username)[0]
+
+    console.log(this.currentUser)
+  }
 }
