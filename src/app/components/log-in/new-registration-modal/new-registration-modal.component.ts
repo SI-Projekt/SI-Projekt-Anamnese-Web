0b8025426054502933818534e5400ef459e42a9f
Steve Carmel <steve2christ@users.noreply.github.com>
2021-01-13 03:03:38 +0100

    wip

---
+++ src/app/components/log-in/new-registration-modal/new-registration-modal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-new-registration-modal',
+  templateUrl: './new-registration-modal.component.html',
+  styleUrls: ['./new-registration-modal.component.css']
+})
+export class NewRegistrationModalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
