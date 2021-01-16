fdb4ac256b0439acbbbd2fe7001497d634bccb00
Steve Carmel <steve2christ@users.noreply.github.com>
2021-01-16 15:30:13 +0100

    wip

---
+++ src/app/components/personal-info-view/allergy-list/allergy-modal/allergy-modal.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core'
+
+@Component({
+  selector: 'app-allergy-modal',
+  templateUrl: './allergy-modal.component.html',
+  styleUrls: ['./allergy-modal.component.css']
+})
+export class AllergyModalComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/components/personal-info-view/disease-list/disease-modal/disease-modal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-disease-modal',
+  templateUrl: './disease-modal.component.html',
+  styleUrls: ['./disease-modal.component.css']
+})
+export class DiseaseModalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
