97b5290e0daa8108c3b7dadb97590a1df2a0d4fe
MistraForest <mistraf16@yahoo.fr>
2021-01-22 04:44:29 +0100

    Prod environment set

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
 export const environment = {
-  production: true
+  production: false,
+
+  // Paths:
+  // baseUrl: 'http://54.145.7.15:8080', // server
+  baseUrl: 'ec2-3-84-206-109.compute-1.amazonaws.com:8080' // rds
+  // baseUrl: 'http://localhost:8080' // local
+
 }
