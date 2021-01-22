930e455174c8e2567443fdcbf5e5b513330638d7
MistraForest <mistraf16@yahoo.fr>
2021-01-22 03:52:13 +0100

    add dist ressource

--- scripts/deploy_script.sh
+++ scripts/deploy_script.sh
@@ -3,4 +3,4 @@
 # configure the bucket to serve static file. May this can be done once.
 aws s3 website s3://si-project-webapp-bucket --index-document index.html --error-document index.html
 # path to be found in angular.json, it is SI-Projekt-Anamnese-Web
-aws s3 sync src s3://si-project-webapp-bucket
+aws s3 sync dist/SI-Projekt-Anamnese-Web s3://si-project-webapp-bucket
