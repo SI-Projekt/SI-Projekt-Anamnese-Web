df8b1db8afd288d2bb7f8d58a0b634ede899676f
MistraForest <mistraf16@yahoo.fr>
2021-01-24 22:16:16 +0100

    Test deployment after bug fixed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,8 +2,6 @@ export const environment = {
   production: true,
 
   // Paths:
-  // baseUrl: 'http://54.145.7.15:8080', // server
-  baseUrl: 'ec2-3-84-206-109.compute-1.amazonaws.com:8080' // rds
-  // baseUrl: 'http://localhost:8080' // local
+  baseUrl: 'ec2-34-207-186-190.compute-1.amazonaws.com:8080' // rds
 
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,14 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: true,
   production: false,
   currentEnvironment: 'Local',
 
-  // Paths:
-  // baseUrl: 'http://54.145.7.15:8080', // server
-  baseUrl: 'ec2-3-84-206-109.compute-1.amazonaws.com:8080' // rds
-  // baseUrl: 'http://localhost:8080' // local
   baseUrl: 'http://localhost:8080'
 
 }
